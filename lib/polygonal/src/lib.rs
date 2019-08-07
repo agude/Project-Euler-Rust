74a0008826790f80cb51c1f3f1f0fb695bdc07ec
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2019-08-06 21:21:05 -0700

    Add Polygonal generators

--- lib/polygonal/src/lib.rs
+++ lib/polygonal/src/lib.rs
@@ -36,6 +36,77 @@ pub fn nth_hexagonal(n: usize) -> u64 {
     return (2 * n * (2 * n - 1) / 2) as u64;
 }
 
+pub struct Polygonals {
+    multiplier: i64,
+    divisor: i64,
+    offset: i64,
+    n: i64,
+}
+
+impl Polygonals {
+    pub fn new(multiplier: i64, divisor: i64, offset: i64, start: i64) -> Polygonals {
+        return Polygonals{
+            multiplier: multiplier,
+            divisor: divisor,
+            offset: offset,
+            n: start,
+        }
+    }
+}
+
+impl Iterator for Polygonals {
+    type Item = i64;
+    fn next(&mut self) -> Option<Self::Item> {
+        let res = Some(self.n * ((self.multiplier * self.n) + self.offset) / self.divisor);
+        self.n += 1;
+        return res;
+    }
+}
+
+pub fn get_triangulars_iterator(start: i64) -> Polygonals {
+    return Polygonals{
+        multiplier: 1,
+        divisor: 2,
+        offset: 1,
+        n: start,
+    }
+}
+
+pub fn get_squares_iterator(start: i64) -> Polygonals {
+    return Polygonals{
+        multiplier: 1,
+        divisor: 1,
+        offset: 0,
+        n: start,
+    }
+}
+
+pub fn get_pentagonals_iterator(start: i64) -> Polygonals {
+    return Polygonals{
+        multiplier: 3,
+        divisor: 2,
+        offset: -1,
+        n: start,
+    }
+}
+
+pub fn get_heptagonals_iterator(start: i64) -> Polygonals {
+    return Polygonals{
+        multiplier: 5,
+        divisor: 2,
+        offset: -3,
+        n: start,
+    }
+}
+
+pub fn get_octagonals_iterator(start: i64) -> Polygonals {
+    return Polygonals{
+        multiplier: 3,
+        divisor: 1,
+        offset: -2,
+        n: start,
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -169,6 +240,76 @@ mod tests {
                 continue;
             }
             assert_eq!(nth_hexagonal(n), *hexagonal);
+        }
+    }
+
+    #[test]
+    fn test_triangular_iterator() {
+        let truth = vec![
+            0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78, 91, 105, 120, 136, 153, 171, 190, 210,
+            231, 253, 276, 300, 325, 351, 378, 406, 435, 465, 496, 528, 561, 595, 630, 666, 703,
+            741, 780, 820, 861, 903, 946, 990, 1035, 1081, 1128, 1176, 1225, 1275, 1326, 1378,
+            1431,
+        ];
+
+        // Test ever number not in the set
+        let iter = get_triangulars_iterator(0);
+        for (n, val) in iter.take(truth.len()).enumerate() {
+            println!("{}: {}", n, val);
+            assert_eq!(truth[n], val);
+        }
+    }
+
+    #[test]
+    fn test_square_iterator() {
+        let truth = vec![0, 1, 4, 9, 16, 25];
+
+        // Test ever number not in the set
+        let iter = get_squares_iterator(0);
+        for (n, val) in iter.take(truth.len()).enumerate() {
+            println!("{}: {}", n, val);
+            assert_eq!(truth[n], val);
+        }
+    }
+
+    #[test]
+    fn test_pentagonal_iterator() {
+        let truth = vec![
+            0, 1, 5, 12, 22, 35, 51, 70, 92, 117, 145, 176, 210, 247, 287, 330, 376, 425, 477, 532,
+            590, 651, 715, 782, 852, 925, 1001, 1080, 1162, 1247, 1335, 1426, 1520, 1617, 1717,
+            1820, 1926, 2035, 2147, 2262, 2380, 2501, 2625, 2752, 2882, 3015, 3151, 3290, 3432,
+            3577, 3725, 3876, 4030,
+        ];
+
+        // Test ever number not in the set
+        let iter = get_pentagonals_iterator(0);
+        for (n, val) in iter.take(truth.len()).enumerate() {
+            println!("{}: {}", n, val);
+            assert_eq!(truth[n], val);
+        }
+    }
+
+    #[test]
+    fn test_heptagonals_iterator() {
+        let truth = vec![0, 1, 7, 18, 34, 55];
+
+        // Test ever number not in the set
+        let iter = get_heptagonals_iterator(0);
+        for (n, val) in iter.take(truth.len()).enumerate() {
+            println!("{}: {}", n, val);
+            assert_eq!(truth[n], val);
+        }
+    }
+
+    #[test]
+    fn test_octagonals_iterator() {
+        let truth = vec![0, 1, 8, 21, 40, 65];
+
+        // Test ever number not in the set
+        let iter = get_octagonals_iterator(0);
+        for (n, val) in iter.take(truth.len()).enumerate() {
+            println!("{}: {}", n, val);
+            assert_eq!(truth[n], val);
         }
     }
 }

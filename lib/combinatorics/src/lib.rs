46aaeb665a99c1f8bf8f4376b64c17242f715b33
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2019-08-09 21:23:40 -0700

    Add Ratio factorial

--- lib/combinatorics/src/lib.rs
+++ lib/combinatorics/src/lib.rs
@@ -1,14 +1,34 @@
+use std::cmp::max;
+use std::cmp::min;
+
 pub fn n_choose_k(n: u32, k: u32) -> u128 {
     // We define n choose k for k > n as 0, that is, there are no way to choose more items than
     // exist in a set.
     if k > n {
         return 0;
     }
+    // There is only one way to pick all the objects from a set.
+    if k == n {
+        return 1;
+    }
 
-    let numerator = factorial(n);
-    let denominator = factorial(k) * factorial(n-k);
+    // Factorial functions overflow very easily, so we cancel first
+    let bottom = max(k, n-k);
+    let other  = min(k, n-k);
 
-    return numerator / denominator;
+    let ratio = factorial_ratio(n, bottom);
+    let denominator = factorial(other);
+
+    return ratio / denominator;
+}
+
+pub fn factorial_ratio(top: u32, bottom: u32) -> u128 {
+    if top > bottom {
+        let start = (bottom + 1) as u128;
+        return (start..=top as u128).product();
+    }
+    // TODO [#2]:
+    return 0;
 }
 
 pub fn factorial(n: u32) -> u128 {
@@ -41,5 +61,12 @@ mod tests {
         assert_eq!(n_choose_k(5, 3), 10);
         assert_eq!(n_choose_k(0, 1), 0);
         assert_eq!(n_choose_k(34, 17), 2_333_606_220);
+    }
+
+    #[test]
+    fn test_factorial_ratio() {
+        assert_eq!(factorial_ratio(10, 5), 30_240);
+        assert_eq!(factorial_ratio(1000, 999), 1000);
+        assert_eq!(factorial_ratio(1, 100), 0);
     }
 }
